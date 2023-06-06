added dlnMgdlnr param and fixed units

cnc/cat.py
@@ -166,56 +166,35 @@ class cluster_catalogue:
                 ## add a seperate zmin threshold, for observed clusters.
                 if (spt_catalog["xi"][i] > threshold) and (spt_catalog['redshift'][i]>self.cnc_params["z_min"]):
                     indices_catalog.append(i)
-            # exit(0)
+
 
 
 
             self.catalogue["z"] = np.asarray(spt_catalog['redshift'][indices_catalog])
-            # print('z:',self.catalogue["z"][:10])
+
             indices_no_z = np.where(self.catalogue["z"] == 0.)[0]
 
-            # print('indices_no_z',indices_no_z)
             self.catalogue["z"][indices_no_z] = None
 
             self.catalogue["z_std"] = np.asarray(spt_catalog['redshift_err'][indices_catalog])
-            # print('z_std:',len(self.catalogue["z_std"]))
-            # print('z_std:',self.catalogue["z_std"][:10])
-
 
             self.catalogue["xi"] = np.asarray(spt_catalog['xi'][indices_catalog])
-
-            # print('xi:',len(self.catalogue["xi"]))
-            # print('xi:',self.catalogue["xi"][:10])
 
             self.catalogue_patch['xi'] = np.zeros(len(self.catalogue['xi'])).astype(np.int)
             for id,field in enumerate(spt_catalog['field'][indices_catalog]):
                 self.catalogue_patch['xi'][id] = SPTfieldNames.index(field)
-            # print('patches xi:',len(self.catalogue_patch["xi"]))
-            # print('patches xi:',self.catalogue_patch["xi"][:10])
 
-            # print('self.obs_select',self.obs_select)
-
-            # print('threshold =',self.cnc_params['obs_select_threshold'])
-            # exit(0)
-
-            # print(self.observables)
-            # exit(0)
             if 'Yx' in self.observables[0]:
                 # print('adding Yx data')
                 self.catalogue["Yx"] = np.asarray(spt_catalog['Yx_fid'][indices_catalog])
-                # print()
                 self.catalogue["Yx_std"] = np.asarray(spt_catalog['Yx_err'][indices_catalog])
+                self.catalogue["r500"] = np.asarray(spt_catalog['r500'][indices_catalog])
+
 
                 indices_no_Yx = np.where(self.catalogue["Yx"] <= 0.)[0]
                 self.catalogue["Yx"][indices_no_Yx] = None
-
-
                 self.catalogue_patch["Yx"] = np.arange(len(self.catalogue["Yx"])).astype(np.int)# index of all clusters.
-                # print(self.catalogue["Yx"])
-                # exit(0)
-                # print(self.catalogue["Yx_std"])
-                # print(self.catalogue_patch["Yx"])
-                # exit(0)
+
 
             if 'WLMegacam' or 'WLHST' in self.observables[0]:
                 # WL simulation calibration data --  same as Bocquet's code

cnc/cosmo.py
@@ -208,8 +208,35 @@ class cosmology_model:
             spt_zs = np.linspace(1e-5,5,200)
             self.spt_ln1pzs = np.log(1.+spt_zs)
             self.spt_lndas_hmpc = np.log(self.background_cosmology_sptref.angular_diameter_distance(spt_zs).value*self.classy_sptref.h())
-            # print('spt_zs,spt_das',self.spt_ln1pzs[:5],self.spt_lndas_hmpc[:5])
-            # print('spt_zs,spt_das',other_params['cosmology'].spt_ln1pzs[:5],other_params['cosmology'].spt_lndas_hmpc[:5])
+
+
+            # cosmologyRef = {'Omega_m':.272, 'Omega_l':.728, 'h':.702, 'w0':-1, 'wa':0}
+            spt_cosmoRef_masscal = {'Omega_m':.272,
+                                    'Omega_l':.728,
+                                    'h':.702,
+                                    'w0':-1.,
+                                    'wa':0,
+                                    # "Ob0":
+                                    }
+            self.spt_cosmoRef_masscal = spt_cosmoRef_masscal
+            self.classy_sptref.set({
+                                   'H0': spt_cosmoRef_masscal["h"]*100.,
+                                   'omega_b': self.cosmo_params["Ob0"]*spt_cosmoRef_masscal["h"]**2,
+                                   'omega_cdm': (spt_cosmoRef_masscal['Omega_m']-self.cosmo_params["Ob0"])*spt_cosmoRef_masscal["h"]**2,
+                                   'output': ' '
+                                   }
+                                   )
+            # print('recomputing spt ref cosmo')
+            self.classy_sptref.compute()
+            # print('spt ref cosmo recomputed')
+            self.background_cosmology_sptref_masscal = classy_sz(self.classy_sptref)
+            # self.background_cosmology_sptref.H0.value = (self.classy_sptref.h()*100.)
+
+            spt_zs_masscal = np.linspace(1e-5,5,200)
+            self.spt_ln1pzs_masscal = np.log(1.+spt_zs_masscal)
+            self.spt_lndas_hmpc_masscal = np.log(self.background_cosmology_sptref_masscal.angular_diameter_distance(spt_zs_masscal).value*self.classy_sptref.h())
+
+
 
 
             # print('spt cosmoref re-computed',self.background_cosmology_sptref.H0.value/100.)