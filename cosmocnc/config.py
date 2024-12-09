c6ea7cb404fe741429fb510143c2e54cc08004a0
borisbolliet <boris.bolliet@gmail.com>
2024-12-09 17:29:28 +0000

    Update config.py

--- cosmocnc/config.py
+++ cosmocnc/config.py
@@ -25,7 +25,7 @@ env_var = 'PATH_TO_COSMOPOWER_ORGANIZATION'
 
 if os.getenv(env_var):
     path_to_cosmopower_organization = os.getenv(env_var)
-    print(f"{env_var} is already set to: {path_to_cosmopower_organization}")
+    # print(f"{env_var} is already set to: {path_to_cosmopower_organization}")
 else:
     print(f"Warning: {env_var} is not set.")
     print("defaulting to ", path_to_cosmopower_organization)
