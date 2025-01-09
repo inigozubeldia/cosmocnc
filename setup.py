e712f3af21c5826252ad03693feb4a2219b359c6
Íñigo Zubeldia <49374875+inigozubeldia@users.noreply.github.com>
2025-01-09 21:16:22 +0100

    Added version

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 from setuptools import setup
 
 
-
-
 setup(
     name="cosmocnc",
-    version="0.1",
-    description="Python package for fast cnc",
+    version="1.0",
+    description="Python package for fast cluster number count likelihood computation",
     zip_safe=False,
     packages=["cosmocnc"],
-    author = 'Inigo Zubeldia',
+    author = 'Inigo Zubeldia and Boris Bolliet',
     author_email = 'inigo.zubeldia@ast.cam.ac.uk',
-    url = 'https://github.com/inigozubeldia/cnc',
-    download_url = 'https://github.com/inigozubeldia/cnc',
+    url = 'https://github.com/inigozubeldia/cosmocnc',
+    download_url = 'https://github.com/inigozubeldia/cosmocnc',
     package_data={
         # "specdist": ["data/*txt"],
         #"data/ct_database/case_1_040520/*txt"]#,
